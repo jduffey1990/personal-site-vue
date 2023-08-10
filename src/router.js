3e7ecb282fb3a4e58b451d5dc10f75bd67e12847
jduffey1990 <jduffey1990@gmail.com>
2023-08-10 13:37:58 -0600

    Updated router to use hash mode

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import IntroSection from './components/IntroSection.vue';
 import AboutSection from './components/AboutSection.vue';
 import ContactSection from './components/ContactSection.vue';
@@ -10,7 +10,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 });
 
